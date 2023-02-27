b34e5553b86e28a2cfba929c0bf4732cbdb53486
Nikhil <nikhilshah.net@hotmail.com>
2023-02-27 10:58:44 +0530

    Day 5 - Api calls

---
+++ Day 5/consumeAPI_Client/consumeAPI_Client/Controllers/SocialMediaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace consumeAPI_Client.Controllers
+{
+    public class SocialMediaController : Controller
+    {
+        public IActionResult PostData()
+        {
+            return View();
+        }
+    }
+}
