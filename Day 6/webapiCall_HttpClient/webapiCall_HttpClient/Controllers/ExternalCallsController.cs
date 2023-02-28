25147e49cd2817a96d6b1d08314d345d443b60d2
Nikhil <nikhilshah.net@hotmail.com>
2023-02-28 10:54:11 +0530

    Http Client calls

---
+++ Day 6/webapiCall_HttpClient/webapiCall_HttpClient/Controllers/ExternalCallsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using webapiCall_HttpClient.Models;
+namespace webapiCall_HttpClient.Controllers
+{
+    public class ExternalCallsController : Controller
+    {
+
+        PostData pObj = new PostData();
+
+        public IActionResult PostData()
+        {
+
+
+            ViewBag.data = pObj.GetPostData();
+            return View();
+        }
+    }
+}
---
+++ Day 6/webapiCall_HttpClient/webapiCall_HttpClient/Models/PostData.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace webapiCall_HttpClient.Models
+{
+    public class PostData
+    {
+
+        public int userId { get; set; }
+        public int Id { get; set; }
+        public string title { get; set; } = "";
+        public string body { get; set; } = "";
+
+
+        List<PostData> data = new List<PostData>();
+        public List<PostData> GetPostData()
+        {
+            var url = "https://jsonplaceholder.typicode.com/posts";
+            HttpClient client = new HttpClient();
+
+            //every calling environment, has a default data format for data, eg. chrome has default xml, ie6 has xml, edge has json, 
+
+            client.DefaultRequestHeaders.Accept.Clear(); //clear the default
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+            
+            //make a call
+            var call = client.GetAsync(url).Result;
+
+            //check if the call is successful
+            if (call.IsSuccessStatusCode)
+            {
+                var postDetails = call.Content.ReadAsAsync<List<PostData>>();
+                postDetails.Wait();
+                data = postDetails.Result;
+                
+            }
+            else
+            {
+                throw new Exception("Call failed, please contact admin");
+            }
+            return data;
+        }
+
+
+    }
+}
