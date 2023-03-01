cf44a9cf1a11cd62b7c3d0781e167027e8514c4f
Nikhil <nikhilshah.net@hotmail.com>
2023-03-01 10:59:51 +0530

    Day 7 - CORS

---
+++ Day 7/shoppingAPI/shoppingAPI/Models/EF/ShoppingDbContext.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace shoppingAPI.Models.EF;
+
+public partial class ShoppingDbContext : DbContext
+{
+    public ShoppingDbContext()
+    {
+    }
+
+    public ShoppingDbContext(DbContextOptions<ShoppingDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Customer> Customers { get; set; }
+
+    public virtual DbSet<Product> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("server=(localdb)\\MSSQLLocalDB;database=shoppingDB;integrated security=true");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Customer>(entity =>
+        {
+            entity.HasKey(e => e.CId).HasName("PK__customer__D830D4773F1B76DC");
+
+            entity.ToTable("customers");
+
+            entity.Property(e => e.CId)
+                .ValueGeneratedNever()
+                .HasColumnName("cId");
+            entity.Property(e => e.CCity)
+                .HasMaxLength(20)
+                .IsUnicode(false)
+                .HasColumnName("cCity");
+            entity.Property(e => e.CIsActive).HasColumnName("cIsActive");
+            entity.Property(e => e.CName)
+                .HasMaxLength(20)
+                .IsUnicode(false)
+                .HasColumnName("cName");
+            entity.Property(e => e.CWalletBalance).HasColumnName("cWalletBalance");
+        });
+
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.HasKey(e => e.PId).HasName("PK__products__DD36D56240EAC530");
+
+            entity.ToTable("products");
+
+            entity.Property(e => e.PId)
+                .ValueGeneratedNever()
+                .HasColumnName("pId");
+            entity.Property(e => e.PCategory)
+                .HasMaxLength(20)
+                .IsUnicode(false)
+                .HasColumnName("pCategory");
+            entity.Property(e => e.PIsInStock).HasColumnName("pIsInStock");
+            entity.Property(e => e.PName)
+                .HasMaxLength(20)
+                .IsUnicode(false)
+                .HasColumnName("pName");
+            entity.Property(e => e.PPrice).HasColumnName("pPrice");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Day 7/shoppingAPP_MVC/shoppingAPP_MVC/Controllers/ShoppingController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace shoppingAPP_MVC.Controllers
+{
+    public class ShoppingController : Controller
+    {
+        public IActionResult Products()
+        {
+            return View();
+        }
+
+
+        public IActionResult Addproduct()
+        {
+            return View();
+        }
+
+      
+
+
+    }
+}
